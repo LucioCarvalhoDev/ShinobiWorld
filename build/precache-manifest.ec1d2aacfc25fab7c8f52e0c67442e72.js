20bb205bc1846ed1bb3c4cdbc48b32d6fa441e1c
lucio <luciocarvalhodev@gmail.com>
2020-12-10 20:41:00 -0300

    mudanças no git ignore

---
+++ build/precache-manifest.ec1d2aacfc25fab7c8f52e0c67442e72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad1f44eb1fd9b908e3c160bd07c7735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e09f49a9d4b8c0af63c",
+    "url": "/static/css/main.8957fc90.chunk.css"
+  },
+  {
+    "revision": "fc336c5c1257459a21da",
+    "url": "/static/js/2.11f04f6b.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.11f04f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e09f49a9d4b8c0af63c",
+    "url": "/static/js/main.0d8afaad.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
